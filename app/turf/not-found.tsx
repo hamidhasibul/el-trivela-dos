1a56eb0a7f7237b4d7e94eb5e1c1bcaab9d1b928
hasib <hamidhasibul@gmail.com>
2023-10-27 18:33:10 +0600

    minor changes

--- app/turf/not-found.tsx
+++ app/turf/not-found.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import errorMascot from "@/public/icons/error.png";
 
-const NotFound = ({ error }: { error: Error }) => {
+const NotFound = () => {
   return (
     <div className="h-screen bg-gray-200 flex flex-col justify-center items-center">
       <Image src={errorMascot} alt="error" className="w-56 mb-8" />
