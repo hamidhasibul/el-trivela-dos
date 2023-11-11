c6ffda90a0dfc21dadd2d0105cd4f925a0790363
hasib <hamidhasibul@gmail.com>
2023-11-12 01:16:29 +0600

    Worked more on reservation route

--- app/api/turf/[slug]/reserve/route.ts
+++ app/api/turf/[slug]/reserve/route.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { findAvailableFields } from "@/services/turf/findAvailableFields";
+import { Field, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -9,12 +10,14 @@ export async function GET(
   const { searchParams } = new URL(req.url);
 
   const slug = params.slug;
-  const day = searchParams.get("day");
-  const time = searchParams.get("time");
-  const partySize = searchParams.get("partySize");
+  const day = searchParams.get("day") as string;
+  const time = searchParams.get("time") as string;
+  const partySize = searchParams.get("partySize") as string;
 
   const turf = await prisma.turf.findUnique({
     where: { slug },
+
+    select: { fields: true, open_time: true, close_time: true },
   });
 
   if (!turf) {
@@ -34,7 +37,65 @@ export async function GET(
       }
     );
   }
-  return new Response(JSON.stringify({ slug, day, time, partySize }), {
+
+  const searchTimesWithFields = (await findAvailableFields({
+    time,
+    day,
+    turf,
+  })) as Array<{ date: Date; time: string; fields: Field[] }>;
+
+  if (!searchTimesWithFields) {
+    return new Response(JSON.stringify({ message: "Invalid data provided" }), {
+      status: 400,
+    });
+  }
+
+  const searchTimeWithField = searchTimesWithFields.find((t) => {
+    return t.date.toISOString() === new Date(`${day}T${time}`).toISOString();
+  });
+
+  /*   const fieldsCount: { 10: number[]; 12: number[]; 14: number[] } = {
+    10: [],
+    12: [],
+    14: [],
+  };
+
+  searchTimeWithField?.fields.forEach((field) => {
+    if (field.seats === 10) {
+      fieldsCount[10].push(field.id);
+    } else if (field.seats === 12) {
+      fieldsCount[12].push(field.id);
+    } else {
+      fieldsCount[14].push(field.id);
+    }
+  }); */
+
+  const fieldsCount: Record<number, number[]> = {};
+
+  searchTimeWithField?.fields.forEach((field) => {
+    const seats = field.seats;
+
+    if (!fieldsCount[seats]) {
+      fieldsCount[seats] = [];
+    }
+
+    fieldsCount[seats].push(field.id);
+  });
+
+  const fieldToBook: number[] = [];
+  let seatsRemaining = +partySize;
+
+  while (seatsRemaining > 0) {
+    if (fieldsCount[+partySize].length) {
+      fieldToBook.push(fieldsCount[+partySize][0]);
+      fieldsCount[+partySize].shift();
+      seatsRemaining = seatsRemaining - +partySize;
+    } else {
+      break;
+    }
+  }
+
+  return new Response(JSON.stringify({ fieldToBook }), {
     status: 200,
   });
 }
